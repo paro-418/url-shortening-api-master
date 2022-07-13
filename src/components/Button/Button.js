51b84893c0e443e2e6627a43955bfb9e901a7757
Praveen kumar verma <vermapraween6543@gmail.com>
2022-07-13 23:37:12 +0530

    corrected Buton Component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,13 @@
-
-
-const Button = (props) =>{
-    <button type={props.type} onClick={props.callFunction} className = {props.className}>{props.child}</button>
-}
+const Button = (props) => {
+  return (
+    <button
+      type={props.type}
+      onClick={props.callFunction}
+      className={props.className}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
